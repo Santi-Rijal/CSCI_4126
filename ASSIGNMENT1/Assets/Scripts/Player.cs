5504898755dcfb47900d32ce4286acd75de68153
Santi <100055564+Santi-Rijal@users.noreply.github.com>
2023-09-17 22:21:57 -0300

    progress

--- ASSIGNMENT1/Assets/Scripts/Player.cs
+++ ASSIGNMENT1/Assets/Scripts/Player.cs
@@ -1,25 +1,42 @@
+using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using Gyroscope = UnityEngine.InputSystem.Gyroscope;
 
 public class Player : MonoBehaviour {
     
-    [SerializeField] private PlayerInput playerInput;
     [SerializeField] private float playerSpeed = 3f;
 
+    private int _steps;
     private CharacterController _characterController;
-    
+
     private void Awake() {
-        _characterController = GetComponent<CharacterController>();
+        if (SystemInfo.supportsAccelerometer) InputSystem.EnableDevice(Accelerometer.current);
+        if (SystemInfo.supportsGyroscope) InputSystem.EnableDevice(Gyroscope.current);
+
+        _characterController = new CharacterController();
     }
 
     private void Update() {
         HandleMove();
+        LookAround();
     }
 
     private void HandleMove() {
-        var inputVector = playerInput.GetMovementVectorNormalized();
+        if (Accelerometer.current.enabled) {
+            var acceleration = Accelerometer.current.acceleration.ReadValue();
 
-        var moveDir = new Vector3(-inputVector.x, 0f, -inputVector.y);
-        
-        _characterController.Move(moveDir * (Time.deltaTime * playerSpeed));
+            var moveDirection = new Vector3(acceleration.x * (playerSpeed * Time.deltaTime), 0f,
+                -acceleration.z * (playerSpeed * Time.deltaTime));
+            var transformedDirection = transform.TransformDirection(moveDirection);
+
+            _characterController.Move(transformedDirection);
+        }
+    }
+    
+    private void LookAround() {
+        if (Gyroscope.current.enabled) {
+            
+        }
     }
 }
