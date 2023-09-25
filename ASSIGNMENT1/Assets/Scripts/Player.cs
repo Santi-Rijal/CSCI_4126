ecd958c032b1198042f47637da5a705a948af3bc
Santi <100055564+Santi-Rijal@users.noreply.github.com>
2023-09-25 19:40:07 -0300

    accelerator working

--- ASSIGNMENT1/Assets/Scripts/Player.cs
+++ ASSIGNMENT1/Assets/Scripts/Player.cs
@@ -13,13 +13,16 @@ public class Player : MonoBehaviour {
     private void Awake() {
         if (SystemInfo.supportsAccelerometer) InputSystem.EnableDevice(Accelerometer.current);
         if (SystemInfo.supportsGyroscope) InputSystem.EnableDevice(Gyroscope.current);
+        InputSystem.EnableDevice(StepCounter.current);
 
-        _characterController = new CharacterController();
+        _characterController = GetComponent<CharacterController>();
+        _steps = 0;
     }
 
     private void Update() {
         HandleMove();
-        LookAround();
+        //LookAround();
+        StepsUpdate();
     }
 
     private void HandleMove() {
@@ -27,15 +30,33 @@ public class Player : MonoBehaviour {
             var acceleration = Accelerometer.current.acceleration.ReadValue();
 
             var moveDirection = new Vector3(acceleration.x * (playerSpeed * Time.deltaTime), 0f, -acceleration.z * (playerSpeed * Time.deltaTime));
-            var transformedDirection = transform.TransformDirection(moveDirection);
+            //var transformedDirection = transform.TransformDirection(moveDirection);
 
-            _characterController.Move(transformedDirection);
+            if (_characterController == null) {
+                Debug.LogError("CharacterController not found!");
+            }
+            else {
+                _characterController.Move(moveDirection);
+            }
         }
     }
     
     private void LookAround() {
         if (Gyroscope.current.enabled) {
             
+        }
+    }
+
+    private void StepsUpdate() {
+        bool isNotNullAndIsEnabled = StepCounter.current != null && StepCounter.current.enabled;
+
+        if (isNotNullAndIsEnabled) {
+            bool stepsUpdated = StepCounter.current.stepCounter.ReadValue() > _steps;
+            
+            if (stepsUpdated) {
+                _steps = StepCounter.current.stepCounter.ReadValue();
+                Debug.Log(_steps);
+            }
         }
     }
 }
